ace55df08f1b923c0772e3b2fc3e7046d9e3ceaf
tmarr1426 <tmarr1426@gmail.com>
2024-04-03 18:30:36 -0500

    Working on Child Login

--- client/src/components/Authentication/Signup_Child.jsx
+++ client/src/components/Authentication/Signup_Child.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-const Signup_Child = () => {
+const Signup_Child = (props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [parentId, setParentId] = useState("");
 
   const handleChange = (state, value) => {
     switch (state) {
@@ -21,6 +22,9 @@ const Signup_Child = () => {
       case "password":
         setPassword(value);
         break;
+      case "parentId":
+        setParentId(props.id);
+        break;
       default:
         console.log("Something went wrong");
     }
@@ -35,11 +39,11 @@ const Signup_Child = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
+            parentId: 1,
             first: firstName,
             last: lastName,
             email: email,
             password: password,
-            parentId: 1,
           }),
         })
       ).json();
@@ -50,23 +54,41 @@ const Signup_Child = () => {
   };
 
   return (
-    <div>
-      <form style={{ display: "flex", flexDirection: "column" }}>
-        <h2>Sign Up!</h2>
-        <label>First Name</label>
-        <input onChange={(e) => handleChange("first", e.target.value)} />
-        <label>Last Name</label>
-        <input onChange={(e) => handleChange("last", e.target.value)} />
-        <label>Email</label>
-        <input onChange={(e) => handleChange("email", e.target.value)} />
-        <label>Password</label>
-        <input onChange={(e) => handleChange("password", e.target.value)} />
-        <button style={{ margin: "1em" }} type="button" onClick={handleChildSignup}>
-          Sign Up!
-        </button>
-      </form>
-    </div>
+    <>
+      {!parentId ? (
+        <>
+          <div>
+            <form style={{ display: "flex", flexDirection: "column" }}>
+              <h2>Sign Up!</h2>
+              <label>First Name</label>
+              <input onChange={(e) => handleChange("first", e.target.value)} />
+              <label>Last Name</label>
+              <input onChange={(e) => handleChange("last", e.target.value)} />
+              <label>Email</label>
+              <input onChange={(e) => handleChange("email", e.target.value)} />
+              <label>Password</label>
+              <input
+                onChange={(e) => handleChange("password", e.target.value)}
+              />
+              <button
+                style={{ margin: "1em" }}
+                type="button"
+                onClick={handleChildSignup}
+              >
+                Sign Up!
+              </button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <p>Child Account Created!</p>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
-export default Signup_Child
+export default Signup_Child;
--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -40,36 +40,6 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.post("/signup_child", async (req, res) => {
-  try {
-    const user = await prisma.users.create({
-      data: {
-        FirstName: req.body.first,
-        LastName: req.body.last,
-        email: req.body.email,
-        Password: bcrypt.hashSync(req.body.password, 12),
-        // Need to work on figuring out the parentId and how to grab it from the logged in parent
-        parentId: req.parent.id,
-      },
-    });
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: 60 * 60 * 24,
-    });
-
-    res.status(200).json({
-      Mgs: "Success! User created!",
-      User: user,
-      Token: token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      Error: err,
-    });
-  }
-});
-
 //? Logging in a user
 router.post("/login", async (req, res) => {
   try {
@@ -106,6 +76,35 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/signup_child", async (req, res) => {
+  try {
+    const user = await prisma.users.create({
+      data: {
+        FirstName: req.body.first,
+        LastName: req.body.last,
+        email: req.body.email,
+        Password: bcrypt.hashSync(req.body.password, 12),
+        // Need to work on figuring out the parentId and how to grab it from the logged in parent
+        parentId: req.body.parentId,
+      },
+    });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: 60 * 60 * 24,
+    });
+
+    res.status(200).json({
+      Mgs: "Success! User created!",
+      User: user,
+      Token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
 
 router.post("/login_child", async (req, res) => {
   try {
@@ -141,4 +140,5 @@ router.post("/login_child", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
 module.exports = router;
