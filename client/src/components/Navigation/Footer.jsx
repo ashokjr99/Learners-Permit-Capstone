40e6e265063f13c3f82dbab88c6c753415101bb1
tmarr1426 <tmarr1426@gmail.com>
2024-04-18 19:15:07 -0500

    working on parent stats view page

--- client/src/components/Navigation/Footer.jsx
+++ client/src/components/Navigation/Footer.jsx
@@ -7,10 +7,19 @@ const Footer = () => {
   let text = "#C1C8E4";
 
   return (
-    <div className="w3-panel w3-card-4 margin-r-p w3-display-bottommiddle" style={{ display: "flex", gap: "1em"}}>
-      <Link to="/home" style={{color:text}}>Home</Link>
-      <Link to="/about" style={{color:text}}>About</Link>
-      <Link to="/contact_us" style={{color:text}}>Contact</Link>
+    <div
+      className="w3-panel w3-card-4 margin-r-p w3-display-bottommiddle"
+      style={{ display: "flex", gap: "1em" }}
+    >
+      <Link to="/home" style={{ color: text }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: text }}>
+        About
+      </Link>
+      <Link to="/contact_us" style={{ color: text }}>
+        Contact
+      </Link>
     </div>
   );
 };
