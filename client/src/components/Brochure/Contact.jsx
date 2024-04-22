30ebc8206d307b06f893d0f6bf355c5fa542c5e7
NotSanguim <erikengel1209@gmail.com>
2024-04-22 15:14:42 -0400

    button positional updates + colors

--- client/src/components/Brochure/Contact.jsx
+++ client/src/components/Brochure/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="w3-panel w3-card-4 margin-l-p margin-r-p"  style={{marginLeft:"25%"}} >
+      <div className="w3-container">
+        <h2>Contact</h2>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Navigation/Footer.jsx
+++ client/src/components/Navigation/Footer.jsx
@@ -7,13 +7,12 @@ const Footer = () => {
   let text = "#C1C8E4";
 
   return (
-    <div
-      className="w3-panel w3-card-4 margin-r-p w3-display-bottommiddle"
-      style={{ display: "flex", gap: "1em" }}
-    >
-      <Link to="/home" style={{ color: text }}>
-        Home
-      </Link>
+    <div className="w3-panel w3-card-4 margin-l-p margin-r-p w3-display-bottommiddle"  style={{backgroundColor: "#242424", marginLeft:"10%", marginRight:"4%"}} >
+      <div className="w3-container">
+        <div style={{ display: "flex", gap: "1em" }} >
+          {/* <Link to="/home" style={{ color: text }}>
+          Home
+        </Link> */}
       <Link to="/about" style={{ color: text }}>
         About
       </Link>
@@ -21,7 +20,10 @@ const Footer = () => {
         Contact
       </Link>
     </div>
+    </div>
+    </div>
   );
 };
+
 
 export default Footer;
