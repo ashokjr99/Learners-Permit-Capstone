27d8442b356ea4a84fa1a475df74b89a4c3302ac
hussinmarks <hussinmarks6@gmail.com>
2024-04-22 18:05:57 -0400

    monday start up and pull

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,15 @@ const userController = require("./user_controller");
 const statsController = require("./stats_controller");
 const parentController = require("./parent_controller");
 const recoveryController = require("./Account_recovery");
+const settingController = require("./settings");
 
 //? Export controllers
-module.exports = { userController, statsController, parentController, recoveryController };
+module.exports = {
+  userController,
+  statsController,
+  parentController,
+  recoveryController,
+  settingController,
+};
 
 // shortcut to export all in one file
