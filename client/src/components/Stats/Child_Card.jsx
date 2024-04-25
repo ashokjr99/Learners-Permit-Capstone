f65016da6746285da73da19ad903976f4ad71f37
tmarr1426 <tmarr1426@gmail.com>
2024-04-24 20:01:09 -0500

    Working on creating child stats cards

--- client/src/components/Stats/Child_Card.jsx
+++ client/src/components/Stats/Child_Card.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const Child_Card = () => {
   const [results, setResults] = useState([]);
@@ -8,12 +10,13 @@ const Child_Card = () => {
   const [drives, setDrives] = useState(0);
   const [dayHours, setDayHours] = useState(0);
   const [nightHours, setNightHours] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     const getStats = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8081/stats/child_stats`,
+          "http://localhost:8081/stats/child_stats",
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("MyToken")}`,
@@ -23,8 +26,8 @@ const Child_Card = () => {
         const json = await response.json();
         console.log(json);
         setResults(json.userStats);
+        setName(json.summaryData.firstName);
         setHours(json.summaryData.totalHours);
-        setDrives(json.summaryData.totalDrives);
         setDayHours(json.summaryData.totalDayHours);
         setNightHours(json.summaryData.totalNightHours);
       } catch (err) {
@@ -32,12 +35,34 @@ const Child_Card = () => {
       }
     };
     getStats();
-  });
-
+  }, []);
 
   return (
     <div>
-      <Card variant="outlined"></Card>
+      {results.map((result) => {
+        console.log("Babaloo", result);
+        return (
+          <Card
+            key={result.id}
+            style={{ width: "40em", margin:"1em" }}
+          >
+            <CardContent>
+              <Typography sx={{ fontSize: 26 }} color="#244855" gutterBottom>
+                {result.FirstName}
+              </Typography>
+              <Typography sx={{ fontSize: 26 }} color="#244855" gutterBottom>
+                Total Hours Drive: {result.hours}
+              </Typography>
+              <Typography sx={{ fontSize: 26 }} color="#244855" gutterBottom>
+                Total Day Hours Driven: {result.dayHours}
+              </Typography>
+              <Typography sx={{ fontSize: 26 }} color="#244855" gutterBottom>
+                Total Night Hours Driven: {result.nightHours}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
   );
 };
