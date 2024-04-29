c821660c5bd8cb58fb4cfe0b32ec8541df221a26
tmarr1426 <tmarr1426@gmail.com>
2024-04-29 18:49:36 -0500

    Working Code! Working on Styling!!

--- client/src/components/Stats/check_stats_folder_components/DayOrNightFilter.jsx
+++ client/src/components/Stats/check_stats_folder_components/DayOrNightFilter.jsx
@@ -4,6 +4,7 @@ const DayOrNightFilter = ({ setTime }) => {
       <select
         className="dayTimeFilter"
         onChange={(e) => setTime(e.target.value)}
+        style={{boxShadow: "4px 4px 4px #000000"}}
       >
         <option>All</option>
         <option>Day</option>
